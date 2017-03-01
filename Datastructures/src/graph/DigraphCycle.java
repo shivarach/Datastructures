2a4957eaa9b4a90e056addcd60ce6557ca6b947f
Shiva <shiva.rachakonda139@gmail.com>
2017-03-01 22:06:30 +0530

    topological sort

---
+++ Datastructures/src/graph/DigraphCycle.java
@@ -0,0 +1,58 @@
+package graph;
+
+/**
+ * T: O(E + V)
+ * @author Shiva
+ *
+ */
+public class DigraphCycle {
+
+	public boolean[] visited;
+	public boolean[] recStack;
+	public DirectedGraph g;
+
+	public DigraphCycle(DirectedGraph g) {
+		this.g = g;
+		visited = new boolean[g.V()];
+		recStack = new boolean[g.V()];
+	}
+
+	public boolean isCyclic() {
+		for (int i = 0; i < g.V(); i++) {
+			if (!visited[i] && isCyclic(i))
+				return true;
+		}
+		return false;
+	}
+
+	private boolean isCyclic(int v) {
+
+		visited[v] = true;
+		recStack[v] = true;
+		for (int w : g.getAdjacentVertices(v)) {
+			if (!visited[w])
+				return isCyclic(w);
+			else if (recStack[w])
+				return true;
+		}
+
+		// since it is dfs unwinding it is not part of cycle
+		recStack[v] = false;
+		return false;
+	}
+
+	public static void main(String[] args) {
+		DirectedGraph g = new DirectedGraph(5);
+		g.addEdge(0, 0);
+		g.addEdge(1, 2);
+		g.addEdge(2, 3);
+		g.addEdge(3, 0);
+		g.addEdge(0, 2);
+		g.addEdge(3, 1);
+		 
+
+		DigraphCycle dg = new DigraphCycle(g);
+		System.out.println(dg.isCyclic());
+	}
+
+}
---
+++ Datastructures/src/graph/TopologicalOrder.java
@@ -0,0 +1,52 @@
+package graph;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TopologicalOrder {
+	
+	private boolean[] visited;
+	private Queue<Integer> q = new LinkedList();
+	private DirectedGraph g;
+	
+	public TopologicalOrder(DirectedGraph g) {
+		visited = new boolean[g.V()];
+		this.g = g;
+	}
+	
+	public Iterable<Integer> getTopologicalOrder() {
+		if(! new DigraphCycle(g).isCyclic())
+			dfs(g, 0);
+		return q;
+	}
+
+	private void dfs(DirectedGraph g, int i) {
+		visited[i] = true;
+		for(int w : g.getAdjacentVertices(i)) {
+			if(!visited[w]) {
+				visited[w] = true;
+				dfs(g, w);
+			}
+		}
+		q.add(i);//adding in post order gives topological order
+	}
+
+	public static void main(String[] args) {
+		DirectedGraph g = new DirectedGraph(4);
+		g.addEdge(0, 1);
+		g.addEdge(1, 2);
+		g.addEdge(2, 3);
+		//g.addEdge(3, 0);
+		g.addEdge(0, 2);
+		
+		TopologicalOrder tp = new TopologicalOrder(g);
+		Iterable<Integer> itr = tp.getTopologicalOrder();
+		if(!itr.iterator().hasNext())
+			System.out.println("Graph has cycle! Topological order is not possible!");
+		for(int i : itr) {
+			System.out.print(i + "  ");
+		}
+		System.out.println();
+	}
+
+}
